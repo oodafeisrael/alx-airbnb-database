69cbcc2bedaf415856e028a66e7479820eae676c
oodafeisrael <oodafeisrael@yahoo.com>
2025-07-10 20:45:37 +0100

    updated sql file

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -16,8 +16,21 @@ SELECT
     p.property_id,
     p.name AS property_name,
     COUNT(b.booking_id) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
+FROM properties p
+LEFT JOIN bookings b ON p.property_id = b.property_id
+GROUP BY p.property_id, p.name
+ORDER BY booking_rank;
+
+
+/*
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    COUNT(b.booking_id) AS total_bookings,
     RANK() OVER (ORDER BY COUNT(b.booking_id) DESC) AS booking_rank
 FROM properties p
 LEFT JOIN bookings b ON p.property_id = b.property_id
 GROUP BY p.property_id, p.name
 ORDER BY booking_rank;
+*/
