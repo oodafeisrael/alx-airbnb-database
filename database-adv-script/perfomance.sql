6b3da01bdff938d58b70b2ec80a8fb5c8b4bf578
oodafeisrael <oodafeisrael@yahoo.com>
2025-07-11 19:32:16 +0100

    sql file updated

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,59 @@
+-- Initial query to retrieve all bookings with related user, property, and payment data
+
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
+    b.created_at AS booking_date,
+
+    -- User details
+    u.user_id,
+    u.first_name || ' ' || u.last_name AS user_name,
+    u.email,
+    u.phone_number,
+    u.role,
+
+    -- Property details
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    p.pricepernight,
+
+    -- Payment details
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON b.booking_id = pay.booking_id
+
+ORDER BY b.created_at DESC;
+
+
+-- Performance Analysis
+EXPLAIN ANALYZE
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    u.email,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_method,
+    pay.payment_date
+FROM bookings b
+JOIN users u ON b.user_id = u.user_id
+JOIN properties p ON b.property_id = p.property_id
+LEFT JOIN payments pay ON pay.booking_id = b.booking_id;
